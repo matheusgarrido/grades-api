2b2ed48a391ca64070a6f0918064afdb884c6424
Matheus <matheusgarrido10@hotmail.com>
2020-11-09 11:33:29 -0300

    Commit DB connection

--- models/index.js
+++ models/index.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const db = {};
 db.mongoose = mongoose;
 db.url =
-  'mongodb+srv://garridomat:ekWQcdbji3ZyQhB1@desafio4.0rkfw.mongodb.net/Desafio4?retryWrites=true&w=majority';
+  'mongodb+srv://root:FSoWFnBJwXxukcii@desafio4.0rkfw.mongodb.net/Desafio4?retryWrites=true&w=majority';
 
 export { db };
