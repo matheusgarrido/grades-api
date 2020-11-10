dcc69aae22c4e576301b9124a0dd5548c071dcf3
Matheus <matheusgarrido10@hotmail.com>
2020-11-10 16:23:10 -0300

    Cors origin

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin:
+      'http://localhost:3000' || 'https://desafio4-grade-app.herokuapp.com/',
   })
 );
 // app.use('/grade');
